b863811fa7103d01a2cc223dd347a982b6e69935
reepoll <rappoli.chris@gmail.com>
2015-06-06 11:02:07 -0400

    Resuming Shiny project experiment

--- ShinyScratchPad/server.R
+++ ShinyScratchPad/server.R
@@ -2,6 +2,8 @@ library(shiny)
 
 shinyServer(
         function(input, output){
-                
+        output$oid1 = renderPrint({input$id1}) 
+        output$oid2 = renderPrint({input$checkid})
+        output$odate = renderPrint({input$dateid})
         }
 )
--- ShinyScratchPad/ui.R
+++ ShinyScratchPad/ui.R
@@ -1,12 +1,27 @@
 library(shiny)
 
+#take the inputs
 shinyUI(pageWithSidebar(
         headerPanel('Illustrating Inputs'),
         sidebarPanel(
                 numericInput('id1', 'Numeric input panel', 2, min=0, max=5, step=1),
-                checkboxGroupInput("idcheck", 'Check the boxes', c('Value 1'=1, "Value 2"="hello", "Value 3"=3)),
+                checkboxGroupInput('checkid', 'Check the boxes', 
+                                   c('Value 1'=1, 
+                                     'Value 2'="hello", 
+                                     'Value 3'=3)),
                 dateInput("dateid", "Enter Date:")    
         ),
+        #grab the outputs
         mainPanel(
-        )
+                h3('Illustrating Outputs'),
+                h4('You entered'),
+                verbatimTextOutput('oid1'),
+                h4('You entered'),
+                verbatimTextOutput('oid2'),
+                h4('You entered'),
+                verbatimTextOutput('odate')
+        )    
 ))
+
+
+
